e2467c94af534aea4d652d78a6469b2928046f6b
Brad Johnson <bradsk88@gmail.com>
2021-11-06 23:52:23 -0600

    Better support for deployed AND local

--- app/src/app/app-routing.module.ts
+++ app/src/app/app-routing.module.ts
@@ -4,8 +4,8 @@ import {ClaimDisplayNameComponent} from "./claim-display-name/claim-display-name
 import {ShowFrequencyResponsesComponent} from "./show-frequency-responses/show-frequency-responses.component";
 
 const routes: Routes = [
-  {path: "app", component: ShowFrequencyResponsesComponent},
-  {path: "app/display-name/claim", component: ClaimDisplayNameComponent}
+  {path: "view", component: ShowFrequencyResponsesComponent},
+  {path: "display-name/claim", component: ClaimDisplayNameComponent}
 ];
 
 @NgModule({
